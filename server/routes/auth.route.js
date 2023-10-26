d4bd38d97d4bbd1737703ec5c255c78d894a796a
usama7365 <usamaaamirsohail@gmail.com>
2023-10-26 17:28:06 +0500

    updated code improve styling

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/auth.controller');
 const { PostLoginSchema } = require("../controllers/Login.controllers");
 const { PostSignupSchema } = require("../controllers/signup.controller");
-const resetController = require('../controllers/reset.controllers');
 
 
 
